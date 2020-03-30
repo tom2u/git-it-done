2f6bddd75b7a838ae668c5014e29b4485961352a
tom2u <tbrodhead@2u.com>
2020-03-30 14:23:50 -0500

    6.2.3

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,12 +2,13 @@ var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
-    // make a request to the url
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
+      console.log(response);
       response.json().then(function(data) {
         console.log(data);
       });
     });
-};
-
-getUserRepos();
+  };
+  
+  getUserRepos("lernantino");
