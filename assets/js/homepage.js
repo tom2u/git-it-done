d1ecec074fde6bb374b472a4ff127d0970206b9c
tom2u <tbrodhead@2u.com>
2020-03-30 15:34:04 -0500

    6.2.4

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,30 @@
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
+
     // make a get request to url
     fetch(apiUrl).then(function(response) {
-      console.log(response);
-      response.json().then(function(data) {
+        console.log(response);
+        response.json().then(function(data) {
         console.log(data);
-      });
+        });
     });
-  };
+};
   
-  getUserRepos("lernantino");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
